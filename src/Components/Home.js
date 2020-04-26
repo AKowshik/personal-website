70fede29c6613dcd0c4c38da2b210d3cc139f891
AKowshik <ankith.kowshik@gmail.com>
2020-04-26 20:36:24 +0530

    Fixed footer

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Navbar, Nav, Icon, Footer, Divider } from 'rsuite';
+import { Navbar, Nav, Icon, Divider } from 'rsuite';
+import {Image} from 'semantic-ui-react';
 import '../index.css'
 
 class MyHome extends Component {
@@ -7,57 +8,62 @@ class MyHome extends Component {
     render() {
         return (
             <div>
-                <div className='myheader'>
-                    {this.navbarTop()}
-                </div>
-                <div className="mylinks">
-                    {this.weblinks()}
-                </div>
-                <div className='myfooter'>
-                    {this.footer()}
-                </div>
+                {this.navbarTop()}
+
+                {this.weblinks()}
+                
+                {this.footer()}
+
             </div>
         )
     }
 
     navbarTop = () => {
         return (
-            <Navbar appearance="subtle">
-                <Navbar.Body>
-                    <Nav>
-                        <Nav.Item>Ankith C K</Nav.Item>
-                        <Nav.Item>Projects</Nav.Item>
-                        <Nav.Item>Blog</Nav.Item>
-                    </Nav>
-                    <Nav pullRight>
-                        <Nav.Item>Contact</Nav.Item>
-                    </Nav>
-                </Navbar.Body>
-            </Navbar>
+            <div className='myheader'>
+                    <Navbar appearance="subtle">
+                        <Navbar.Body>
+                            <Nav>
+                                <Nav.Item>Ankith C K</Nav.Item>
+                                <Nav.Item>Projects</Nav.Item>
+                                <Nav.Item>Blog</Nav.Item>
+                            </Nav>
+                            <Nav pullRight>
+                                <Nav.Item>Contact</Nav.Item>
+                            </Nav>
+                        </Navbar.Body>
+                    </Navbar>
+            </div>
         );
     }
 
     weblinks = () => {
         return (
-            <Nav vertical >
-                <Nav.Item href='//github.com/AKowshik' target='_blank' icon={<Icon icon="github" size='lg' />}></Nav.Item>
-                <Nav.Item href='//linkedin.com/in/ankith-c-kowshik-306860168/' target='_blank' icon={<Icon icon='linkedin' size='lg' />}></Nav.Item>
-                <Nav.Item href='//www.instagram.com/ankith_c_kowshik/' target='_blank' icon={<Icon icon='instagram' size='lg' />}></Nav.Item>
-            </Nav>
+            <div className="mylinks">
+                <Nav vertical >
+                    <Nav.Item href='//github.com/AKowshik' target='_blank' icon={<Icon icon="github" size='lg' />}></Nav.Item>
+                    <Nav.Item href='//linkedin.com/in/ankith-c-kowshik-306860168/' target='_blank' icon={<Icon icon='linkedin' size='lg' />}></Nav.Item>
+                    <Nav.Item href='//www.instagram.com/ankith_c_kowshik/' target='_blank' icon={<Icon icon='instagram' size='lg' />}></Nav.Item>
+                </Nav>
+            </div>
         )
     }
 
     footer = () => {
         return (
-            <Footer style={{ textAlign: "center"}}>
+            <div className='myfooter'>
                     <a href='//github.com/AKowshik' target='_blank'>GitHub</a>
                     <Divider vertical />
                     <a href='//linkedin.com/in/ankith-c-kowshik-306860168/' target='_blank'>LinkedIn</a>
                     <Divider vertical />
                     <a href='//www.instagram.com/ankith_c_kowshik/' target='_blank'>Instagram</a>
-            </Footer>
+            </div>
 
         )
     }
 }
+
+    
+
+
 export default MyHome;
