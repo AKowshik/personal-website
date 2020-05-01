b4c39324112a9c76454bfaeb9f3abcc08bef47f1
AKowshik <ankith.kowshik@gmail.com>
2020-05-01 22:11:39 +0530

    Restructured files and their contents

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import MyHome from './Components/Home';
+import MyHome from './Components/Home/Home';
 
 
 function App() {
